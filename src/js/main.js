e339a0cb303234c5c7246b17ad440d68abc36295
Yurii <pidlypniyuyra@gmai.com>
2021-11-18 16:48:16 +0200

    добавлено секцію about

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,23 @@ burger.addEventListener('click',()=>{
   burger.classList.toggle('active')
   menu.classList.toggle('active')
   body.classList.toggle('lock')
-
 })
 //----------------------
+//Video
+let buttonPlay = document.querySelector('.video__play')
+let videoBlock = document.querySelector('.about-section__video')
+let video = document.querySelector('.video__block');
 
+buttonPlay.addEventListener('click',()=>{
+  videoBlock.classList.toggle('active');
+  buttonPlay.classList.toggle('active');
+  video.play();
+  video.controls = true
+})
+video.addEventListener('click',()=>{
+  videoBlock.classList.toggle('active');
+  buttonPlay.classList.toggle('active');
+  video.controls = false;
+  video.pause()
+})
+//-----------------------
