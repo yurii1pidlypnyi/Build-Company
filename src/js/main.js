3f1314717703268c38510ce6b8b8299f533adf81
Yurii <pidlypniyuyra@gmai.com>
2021-11-18 11:23:57 +0200

    Добавлено адаптив до шапки

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+//Burger Menu
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+let body = document.body;
+
+burger.addEventListener('click',()=>{
+  burger.classList.toggle('active')
+  menu.classList.toggle('active')
+  body.classList.toggle('lock')
+
+})
+//----------------------
+
