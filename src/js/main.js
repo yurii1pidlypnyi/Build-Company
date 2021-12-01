faf6c87b7cfa6b371261f2a38da40891eab44221
Yurii <pidlypniyuyra@gmai.com>
2021-12-01 12:01:50 +0200

    Закінчено сторінку work-1

--- src/js/main.js
+++ src/js/main.js
@@ -117,6 +117,37 @@ if (similarSlider) {
     }
   });
 }
+//Hero work-1 2swipers
+const workImages = document.querySelector('.work-images-slider');
+
+if (workImages) {
+  const workSlider = new Swiper('.work-images-nav', {
+    spaceBetween: 20,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      576: {
+        slidesPerView: 6
+      },
+      768: {
+        slidesPerView: 10,
+      }
+    }
+  });
+  const workSlidesNav = new Swiper(workImages, {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".work-images__next",
+      prevEl: ".work-images__prev",
+    },
+    thumbs: {
+      swiper: workSlider,
+    },
+  });
+}
+
 //--------------------------------------
 //Scroll To Top
 const toTop = document.querySelector('.to-top');
